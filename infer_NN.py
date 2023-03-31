6db2f60c7016b5874609695b43fbfd919ba2673e
Abhijit Barman <37418029+abhibarman@users.noreply.github.com>
2023-03-31 15:19:43 +0530

    Update infer_NN.py

--- infer_NN.py
+++ infer_NN.py
@@ -1,38 +1,30 @@
-from clearml import Task
-import tensorflow as tf
-import pickle
+from clearml import Model, Task
+import joblib
+import sys 
 
-#get the task
-tsk = Task.init('sarcasm_detector','TF 2.0 Model Inference','inference')
-task = Task.get_task('caec4160faed4bafb009721f2fcf1e98')
+sys.modules['sklearn.externals.joblib'] = joblib
 
-#load the model
-transformer_model_path = task.models.data['output'][0].get_local_copy()
-model = tf.keras.models.load_model(transformer_model_path)
-print(model.summary())
+tsk = Task.init('sarcasm_detector','SKLEARN model inference ','inference')
 
-#load the tokenizer
-tokenizer_path  = task.artifacts['local file'].get_local_copy()
-with open(tokenizer_path, 'rb') as handle:
-    tokenizer = pickle.load(handle)
+sklearn_model_path = Model(model_id="8c76f34b0c6d47818c1ee70d1708dc2e").get_local_copy()
 
-args = {'sentence' : ["Coworkers At Bathroom Sink Locked In Tense Standoff Over Who Going To Wash Hands Longer","grandma jumps into buick for emergency birdseed run"]}
+sklearn_pipeline = joblib.load(sklearn_model_path)
+
+args = {'sentences' : ["Coworkers At Bathroom Sink Locked In Tense Standoff Over Who Going To Wash Hands Longer","grandma jumps into buick for emergency birdseed run"]}
 tsk.connect(args)
-#tokenize input sentences
-sequences = tokenizer.texts_to_sequences(args['sentence'])
-print(f"Input Sentences :\n{args['sentence']}")
-#pad sequences to max_len
-padded =tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=100, padding='post', truncating='post')
 
-res = model.predict(padded)
-for idx, score in enumerate(res):
+scores = sklearn_pipeline.predict_proba(args['sentences'])
+
+print(f"Input Sentences :\n{args['sentences']}")
+
+for idx, score in enumerate(scores):
     score = score[0]
     if score < 0.5:
         label = 'NORMAL'
         score = 1 - score
     else:
         label = 'SARCASTIC'
-    print(f"Commnet: {args['sentence'][idx]}\nLABEL: {label}\nCERTAINTY: {score:.2f}\n")
+    print(f"Commnet: {args['sentences'][idx]}\nLABEL: {label}\nCERTAINTY: {score:.2f}\n")
 
-#print(res)
 tsk.close()
+    
